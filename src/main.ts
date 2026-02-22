c11510236caa2ad61612f6d335ec2e0e363374ab
elder lira <elderfranca@hotmail.com>
2026-02-21 22:22:07 -0300

    implemented vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,20 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' 
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
 
 
 createApp(App)
 .use(router)
+.use(vuetify)
 .mount('#app')
+

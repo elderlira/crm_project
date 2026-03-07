ea1c36e72a5591d0b52ee65b590c56ece502ae8e
elder lira <elderfranca@hotmail.com>
2026-03-07 04:16:39 -0300

    resolved pinia load problem

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-// import 'vuetify/styles'
+
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css' 
+
+import '@mdi/font/css/materialdesignicons.css'
 
 import * as echarts from 'echarts'
 import VueECharts from 'vue-echarts'
@@ -39,29 +40,18 @@ echarts.use([
   CanvasRenderer
 ])
 
-const app = createApp(App)
-const pinia = createPinia()
 
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+const app = createApp(App)
+
+app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.component('v-chart', VueECharts)
 
 app.mount('#app')
-
-const vuetify = createVuetify({
-    components,
-    directives,
-    themes: {
-      light: {
-
-      },
-    },
-  },
-)
-
-createApp(App)
-.use(router)
-.use(vuetify)
-.use(pinia)
-.mount('#app')
-

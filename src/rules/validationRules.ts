33607698dad91c6d6c717fcbc3681b40e6d1af6a
elder lira <elderfranca@hotmail.com>
2026-04-13 03:09:09 -0300

    global rules

---
+++ src/rules/validationRules.ts
@@ -0,0 +1,13 @@
+export type Rule = (v:any) => boolean | string
+
+export const validationRules = {
+
+    required: (message: string): Rule => (v:any) => !!v || message,
+    min: (length: number) => (v: string): boolean | string => (v && v.length >= length) || `O mínimo de ${length} caracteres`,
+    max: (length: number) => (v: string): boolean | string => (v && v.length <= length) || `O máximo de ${length} caracteres`,
+    email: (v: string): boolean | string => /.+@.+\..+/.test(v) || 'E-mail inválido ',
+    phoneNumber: (v :string): boolean | string => /^0-9/.test(v) || 'Somente número'
+
+}
+
+export type ValidationRulesType = typeof validationRules
